e39ac94609d338ecab19d0de345106f7934f6314
Rafay Junaid <rafooaj11@gmail.com>
2024-02-24 00:34:06 +0500

    Homing bullet and explosion on hit

--- Assets/Scripts/LaserMovement.cs
+++ Assets/Scripts/LaserMovement.cs
@@ -9,17 +9,28 @@ public class LaserMovement : MonoBehaviour
 {
     public TurretGenerator turrGen;
     public Rigidbody2D rb;
+    public bool isDeflected = false;
+    public GameObject explosion;
+    public Transform target;
+    public float angleChangingSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = new Vector2(-1 * turrGen.currSpeed, rb.velocity.y);
         GetComponent<SpriteRenderer>().flipX = true;
+        //rb.velocity = new Vector2(-1 * turrGen.currSpeed, rb.velocity.y);
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 actualHitbox = new Vector2(target.transform.position.x, target.transform.position.y + 1f);
+        Vector2 point2Target = (Vector2)transform.position - actualHitbox;
+        point2Target.Normalize();
+        float value = Vector3.Cross(point2Target, transform.right).z;
+        rb.angularVelocity = angleChangingSpeed * value * -1;
+        rb.velocity = -1 * (point2Target * turrGen.currSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -37,9 +48,24 @@ public class LaserMovement : MonoBehaviour
             if (collision.gameObject.GetComponent<HeroKnight>().isAttacking)
             {
                 Debug.Log("PARRYINGGG");
-                GetComponent<SpriteRenderer>().flipX = false;
-                rb.velocity = new Vector2(-1 * rb.velocity.x, rb.velocity.y);
+                //GetComponent<SpriteRenderer>().flipX = false;
+                isDeflected = true;
+                rb.velocity = new Vector2(turrGen.currSpeed, rb.velocity.y);
+                target = GameObject.FindGameObjectWithTag("Enemy").transform;
             }
+            else 
+            {
+                Instantiate(explosion, transform.position, transform.rotation);
+                turrGen.GenerateLaser();
+                Destroy(this.gameObject);
+            }
+        }
+        
+        if (collision.gameObject.CompareTag("Enemy") && isDeflected)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            turrGen.GenerateLaser();
+            Destroy(this.gameObject);
         }
     }
 }
--- Assets/Sprites/Hero Knight - Pixel Art/Demo/HeroKnight.cs
+++ Assets/Sprites/Hero Knight - Pixel Art/Demo/HeroKnight.cs
@@ -227,7 +227,7 @@ public class HeroKnight : MonoBehaviour
             m_animator.SetTrigger("Hurt");
 
         //Attack
-        else if (Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f && !m_rolling)
+        else if (Input.GetMouseButtonDown(0) && m_timeSinceAttack > 1.5f && !m_rolling) // CHANGED TIME SINCE ATTACK FOR PARRY COOLDOWN
         {
             m_currentAttack++;
 
@@ -235,9 +235,7 @@ public class HeroKnight : MonoBehaviour
             if (m_currentAttack > 3)
                 m_currentAttack = 1;
 
-            // Reset Attack combo if time since last attack is too large
-            if (m_timeSinceAttack > 1.0f)
-                m_currentAttack = 1;
+            // REMOVED RESET COMBO
 
             // Call one of three attack animations "Attack1", "Attack2", "Attack3"
             m_animator.SetTrigger("Attack" + m_currentAttack);
