15b231b393bf11f8015b4ab732fd363946b0ed0c
Huzaaiifa <huzaifafarrukh9800@gmail.com>
2024-02-23 07:43:31 +0500

    working on jump effect (not completed)

--- Assets/Scripts/gravityMechanic.cs
+++ Assets/Scripts/gravityMechanic.cs
@@ -12,6 +12,8 @@ public class gravityMechanic : MonoBehaviour
     bool cooldownON = false;
     float timeCD = 2;
     float timeU = 2;
+
+    public GameObject jumpEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -53,6 +55,8 @@ public class gravityMechanic : MonoBehaviour
         if (Input.GetKeyDown("1") && gravityOn == true && cooldownON == false)
         {
             gravityOn = false;
+//            GameObject temp = Instantiate(jumpEffect, new Vector2(player.transform.position.x,(float)-3.27), transform.rotation);
+//            StartCoroutine(destroyEffect(temp));
             body.gravityScale = (float)-2;
             player.transform.Rotate(180, 0, 0);
         }
@@ -61,9 +65,17 @@ public class gravityMechanic : MonoBehaviour
             gravityOn = true;
             usage = timeU;
             cooldownON = true;
+//            GameObject temp = Instantiate(jumpEffect, new Vector2(player.transform.position.x, (float)3.44), player.transform.rotation);
+//            StartCoroutine(destroyEffect(temp));
             body.gravityScale = 3;
             player.transform.Rotate(-180, 0, 0);
         }
 
+    }
+
+    IEnumerator destroyEffect(GameObject efect)
+    {
+        yield return new WaitForSeconds((float)0.5);
+        Destroy(efect);
     }
 }
