2015e1db65a4822c241b7ca8960858f2e54a7c54
AbdullahS17053 <abdullah17053@gmail.com>
2024-02-23 21:00:58 +0500

    idk

--- Assets/Scripts/PlayerBoundary.cs
+++ Assets/Scripts/PlayerBoundary.cs
@@ -7,7 +7,8 @@ public class PlayerBoundary : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
 
     // Update is called once per frame
